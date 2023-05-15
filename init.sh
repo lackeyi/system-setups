d0ff8194ebc50520e844956450614bf0b6245254
itlackey <itlackey@gmail.com>
2023-05-15 13:12:39 -0500

    working

--- init.sh
+++ init.sh
@@ -4,13 +4,13 @@
 
 sudo apt-get update
 
-sudo apt-get install -y python3
+sudo apt-get install -y python3 pip ansible
 
 # Install Ansible
 
-python3 -m pip install --user ansible
+#python3 -m pip install --user ansible
 
 # Run the playbook
 
-ansible-playbook ~/base_setup.yml
+ansible-playbook base_setup.yaml
 
